1790d8d5a185a3899331342e9223c21cf85abd35
Alfatih Abdelmutalib <35189369+alfatihtalab@users.noreply.github.com>
2022-11-06 16:40:49 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,8 +12,58 @@ import 'package:nyt_top_stories/main.dart';
 import 'package:nyt_top_stories/ui/news_list_page.dart';
 
 void main() {
-  // testWidgets('NewsListWidget has not a title', (tester) async {
-  //   // Test code goes here.
-  //   // await tester.pumpWidget(const NewsListPage());
-  // });
+ testWidgets('StoryDetailWidget has a title, description and author textWidgets', (tester) async {
+    // Test code goes here.
+    const storyDetail =
+    StoryDetail(title: "title",
+        description: "description",
+        author: "author",
+        imageUrl: "imageUrl",
+        webViewUrl: "webUrl");
+
+    Widget testWidget = const MediaQuery(
+        data: MediaQueryData(),
+        child: MaterialApp(home: StoryDetailPage(storyDetail: storyDetail))
+    );
+
+    await tester.pumpWidget(testWidget
+    );
+
+    final titleFinder = find.text("title");
+    final descriptionFinder = find.text("description");
+    final authorFinder = find.text("author");
+
+    expect(titleFinder, findsOneWidget);
+    expect(descriptionFinder, findsOneWidget);
+    expect(authorFinder, findsOneWidget);
+
+  });
+
+  testWidgets('NewsListedCardWidget has a title, description and author textWidgets',
+          (tester) async {
+    // Test code goes here.
+    const storyDetail =
+    StoryDetail(title: "title",
+        description: "description",
+        author: "author",
+        imageUrl: "imageUrl",
+        webViewUrl: "webUrl");
+
+    Widget testWidget = MediaQuery(
+        data: const MediaQueryData(),
+        child: MaterialApp(home: NewsListedCardWidget(storyDetail: storyDetail, onTap: () {  },))
+    );
+
+    await tester.pumpWidget(testWidget
+    );
+
+    final titleFinder = find.text(storyDetail.title!);
+    final descriptionFinder = find.text("description");
+    // final imageFinder = find.image(NetworkImage(storyDetail.imageUrl!));
+
+    expect(titleFinder, findsOneWidget);
+    expect(descriptionFinder, findsOneWidget);
+    // expect(imageFinder, findsOneWidget);
+
+  });
 }
